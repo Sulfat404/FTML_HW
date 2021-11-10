b473c55a52ee5a118acc56bcc7ca6a21f082470d
Sulfat404 <72336851+Sulfat404@users.noreply.github.com>
2021-11-10 16:57:22 +0300

    done burgerMenu

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
+
+// carousel
 let i = 1;
     for(let li of carousel.querySelectorAll('li')) {
       li.style.position = 'relative';
       i++;
-      console.log(i);
     }
 
-    let width = 300; // ширина картинки
-    let count = 3; // видимое количество изображений
+let width = 300; // ширина картинки
+let count = 3; // видимое количество изображений
 
-    let list = carousel.querySelector('ul');
-    let listElems = carousel.querySelectorAll('li');
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
 
-    let position = 0; // положение ленты прокрутки
+let position = 0; // положение ленты прокрутки
 
-    carousel.querySelector('.prev').onclick = function() {
-      // сдвиг влево
-      position += width;
-      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
-      position = Math.min(position, 0);
-      list.style.marginLeft = position + 'px';
-    };
+carousel.querySelector('.prev').onclick = function() {
+  // сдвиг влево
+  position += width;
+  // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + 'px';
+};
 
-    carousel.querySelector('.next').onclick = function() {
-      // сдвиг вправо
-      position -= width;
-      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
-      position = Math.max(position, -width * (listElems.length - count));
-      list.style.marginLeft = position + 'px';
-    };
+carousel.querySelector('.next').onclick = function() {
+  // сдвиг вправо
+  position -= width;
+  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+};
+
+// for btn-menu
+
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".header-navbar");
+const closeMenuButton = document.querySelector(".close-button");
+
+const toggleMenu = (event) => {
+  menu.classList.toggle("is-open"); 
+};
+
+menuButton.addEventListener("click", toggleMenu);
+closeMenuButton.addEventListener("click", toggleMenu);
+
+
+
 
